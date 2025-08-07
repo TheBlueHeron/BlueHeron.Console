589d49da7ded801edb68a506f949ac86913ac819
Michael Bakker <michaelbakker1@gmail.com>
2025-08-07 14:00:39 +0200

    Cleanup.

--- CommandLine.Tests/Objects/CommandOptions.cs
+++ CommandLine.Tests/Objects/CommandOptions.cs
@@ -8,14 +8,28 @@ namespace CommandLine.Tests;
 /// </summary>
 internal class CommandOptions
 {
-    private readonly CopyCommand mCopyCommand = new();
-    private readonly DeleteCommand mDeleteCommand = new();
+    private CopyCommand mCopyCommand = null!;
+    private DeleteCommand mDeleteCommand = null!;
 
     [Command("Copy"), Description("Copy a file to a destination.")]
-    public CopyCommand Copy => mCopyCommand;
+    public CopyCommand Copy
+    {
+        get
+        {
+            mCopyCommand ??= new CopyCommand();
+            return mCopyCommand;
+        }
+    }
 
     [Command("Delete"), Description("Delete a file.")]
-    public DeleteCommand Delete => mDeleteCommand;
+    public DeleteCommand Delete
+    {
+        get
+        {
+            mDeleteCommand ??= new DeleteCommand();
+            return mDeleteCommand;
+        }
+    }
 
     [Name("FailSilently"), Description("If true, the command will not throw an error on failure. Default: false.")]
     public bool FailSilently = false;
